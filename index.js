df86335076920d7903df87511a1733daef1f5bd7
OnlyTense <74124620+OnlyTense@users.noreply.github.com>
2021-02-09 10:14:11 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const fs = require('fs');
+
+const prefix = '!';
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./Commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./Commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    const args = message.content.slice(prefix.length).split(/ + /);
+    const command = args.shift().toLowerCase()
+
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args);
+    }
+
+    if(command === 'worldclock'){
+        client.commands.get('worldclock').execute(message, args, Discord);
+    }
+});
+
+
+
+
+client.once('ready', () => {
+    console.log('Bot is now online!');
+});
+
+
+client.login('TOKEN')
