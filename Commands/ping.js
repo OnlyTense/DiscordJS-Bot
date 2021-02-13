cb5ac569ab4d511b8f34c7528512e3a7fabae722
OnlyTense <74124620+OnlyTense@users.noreply.github.com>
2021-02-13 15:06:34 -0600

    Update ping.js

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,7 +1,12 @@
 module.exports = {
     name: "ping",
     description: "This is a ping command!",
-    execute(message, args){
-        message.channel.send('Pong!');
+    execute(message, args, Discord, client){
+        message.channel.send('Pong!')
+        const Embed = new Discord.MessageEmbed()
+        .addField("My Latency is:", `> ${Date.now() - message.createdTimestamp}ms`)
+        .setColor("RANDOM")
+        .setThumbnail('https://media.discordapp.net/attachments/798594125712326737/808933236507017236/ff8c49a85b7fbda0110c66353a9db71e.png')
+        message.channel.send(Embed);
     }
 }
