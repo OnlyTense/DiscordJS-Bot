fc7c6fc09d99ee78db1b585ad682d3622ad4db02
OnlyTense <74124620+OnlyTense@users.noreply.github.com>
2021-02-13 15:09:26 -0600

    Update botstats.js

--- Commands/botstats.js
+++ Commands/botstats.js
@@ -1,5 +1,6 @@
 const time = require('ms')
-
+let os = require ('os').platform();;
+let cpu = require ('os').cpus()[0].model;;
 module.exports = {
     name: "botstats",
     description: "This is a botstats command!",
@@ -8,10 +9,13 @@ module.exports = {
         const uptime = time(client.uptime)
         message.channel.send('These are my statistics.')
 const Embed = new Discord.MessageEmbed()
-.addField("My Latency is:", `${Date.now() - message.createdTimestamp}ms`, inline)
+.addField("My Latency is:", `> ${Date.now() - message.createdTimestamp}ms`)
+.addField("My API Latency is:", `> ${Math.round(client.ws.ping)}ms`)
+.addField("My OS is:", `> ${os}`)
 .setColor("RANDOM")
-.addField("My API Latency is:", `${Math.round(client.ws.ping)}ms`, inline)
-.addField('I have been up for:', uptime)
+.setThumbnail("https://media.discordapp.net/attachments/798594125712326737/808783195940192296/b67e65c2ab0428da786c1f53ebfd4243.png")
+.addField('I have been up for:', `> ${uptime}`)
+.addField("My CPU is:", `> ${cpu}`)
 message.channel.send(Embed);
     }
 }
